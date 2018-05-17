0576243b05af5ea0b6a8fb2184f05be54b399de6
sire-sam <sire-sam@users.noreply.github.com>
2018-05-17 23:43:18 +0200

    End introduction - first example
    From rxjs manual
    http://reactivex.io/rxjs/manual/overview.html#first-examples

--- app/app.js
+++ app/app.js
@@ -1,3 +1,12 @@
-import {Observable} from 'rxjs';
+import { fromEvent } from 'rxjs';
+import { scan, throttleTime, map } from 'rxjs/operators';
 
-console.log(Observable);
+// Introduction - first example
+// http://reactivex.io/rxjs/manual/overview.html#first-examples
+fromEvent(document.querySelector('button'), 'click')
+  .pipe(
+    throttleTime(1000),
+    map(event => event.clientX),
+    scan((count, clientX) => count + clientX, 0),
+  )
+  .subscribe(count => console.log(`clicked ${count} time`));
