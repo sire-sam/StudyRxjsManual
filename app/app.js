5d1e0ffe8616f28096ab342b5c49620f337eebf8
sire-sam <sire-sam@users.noreply.github.com>
2018-05-18 23:04:29 +0200

    End Observer
    From RxJs manual
    http://reactivex.io/rxjs/manual/overview.html#observer

--- app/app.js
+++ app/app.js
@@ -1,87 +1,121 @@
 import { Observable } from 'rxjs';
 
-// Anatomy of an Observable
-// http://reactivex.io/rxjs/manual/overview.html#anatomy-of-an-observable
+// Observer
+// http://reactivex.io/rxjs/manual/overview.html#observer
 
-// Subscribing to Observables
+// Observers are simply a set of callbacks: next, error, and complete
+const observerCallbacks = {
+  next: x => console.log('Observer got next value', x),
+  error: err => console.log('Observer got __error__', err),
+  complete: () => console.log('Oberserver got a __complete__ notification'),
+};
+
+// Observers also be partial
+const partialObserver = {
+  next: x => console.log('Observer without complete got next value', x),
+  error: err => console.log('Observerwithout  got error', err),
+};
+
+// Or just provide callback
+const observerCallBack = (x) => {
+  console.log('Observer callback got next value', x);
+};
+
 //
-// create:argument is a "subscribe" function aka:
-// Calling observable.subscribe with an Observer,
-// the function "subscribe" in Observable.create() is run for that given Observe
-// eslint-disable-next-line
-const obs = Observable.create(function subscribe(observer) {
-  setInterval(
+const observable = Observable.create((observer) => {
+  const id = setInterval(
     () => {
-      console.log('interval execution');
-      observer.next('hi');
+      observer.next('x');
     },
-    1000,
+    250,
   );
-});
-const intervalSubs = obs.subscribe(x => console.log(x));
 
-// Executing Observables
+  return () => {
+    clearInterval(id);
+  };
+});
+
 //
-// next*(error|complete)?
-// zero to infinite Next notifications may be delivered.
-// If either Error or Complete is delivered, then nothing else can be delivered afterwards.
-const obs2 = Observable.create((observer) => {
-  try {
-    observer.next(1);
-    observer.next(2);
-    observer.next(3);
+const subCallbacks = observable.subscribe(observerCallbacks);
+const subPartial = observable.subscribe(partialObserver);
+const subCallback = observable.subscribe(observerCallBack);
+
+setTimeout(() => {
+  subCallbacks.unsubscribe();
+  subPartial.unsubscribe();
+  subCallback.unsubscribe();
+}, 1100);
+
+//
+const observableComplete = Observable.create((observer) => {
+  const id = setInterval(
+    () => {
+      observer.next('y');
+    },
+    250,
+  );
+
+  setTimeout(() => {
+    clearInterval(id);
     observer.complete();
-    observer.next(4); // not delivered
+  }, 1000);
+});
+
+setTimeout(
+  () => {
+    console.log('----complete-----');
+    observableComplete.subscribe(observerCallbacks);
+    observableComplete.subscribe(partialObserver);
+    observableComplete.subscribe(observerCallBack);
+  },
+  1600,
+);
+
+//
+const observableError = Observable.create((observer) => {
+  try {
+    setInerval(
+      () => {
+        observer.next('z');
+      },
+      250,
+    );
   } catch (e) {
     observer.error(e);
   }
 });
-obs2.subscribe(x => console.log(x));
 
-// Disposing Observable Executions
-//
-// subscribe() return Subscription (the ongoing execution), call unsubscribe() cancel the execution
+setTimeout(
+  () => {
+    console.log('----error-----');
 
-// __Important:__
-// --------------
-// Observable must define how to dispose resources of that execution
-// exemple: even if we did unsubscribe for previously defined intervalSubs,
-// interval still exists, see loging of "interval execution" in consol
-setTimeout(() => intervalSubs.unsubscribe(), 4000);
+    // log error
+    observableError.subscribe(observerCallbacks);
 
-//  return a custom unsubscribe function from within function subscribe() to cleanup.
-const obs3 = Observable.create((observer) => {
-  const id = setInterval(
-    () => {
-      console.log('interval too execution');
-      observer.next('hi too');
-    },
-    1000,
-  );
+    // log error
+    setTimeout(() => {
+      observableError.subscribe(partialObserver);
+    }, 500);
 
-  return function unsubscribe() {
-    clearInterval(id);
-  };
-});
-const intervalTooSub = obs3.subscribe(x => console.log(x));
-setTimeout(() => {
-  intervalTooSub.unsubscribe();
-  console.log('no more "interval too" logging');
-}, 4000);
+    // through error (no error callback provide)
+    setTimeout(() => {
+      observableError.subscribe(observerCallBack);
+    }, 1000);
+  },
+  3100,
+);
 
-// __Note__
-// This is pretty simple pattern
-const straightSubscribe = function subscribe(observer) {
-  const id = setInterval(() => {
-    observer.next('hi straight');
-  }, 1000);
-
-  return function unsubscribe() {
-    clearInterval(id);
-  };
-};
-const straightUnsubscribe = straightSubscribe({ next: x => console.log(x) });
-setTimeout(() => straightUnsubscribe(), 4000);
-
-// The reason we use Rx types like Observable, Observer, and Subscription
-// is to get safety (such as the Observable Contract) and composability with Operators.
+// observable.subscribe create an Observer object using the first callback argument as next.
+// All three types of callbacks may be provided as arguments.
+setTimeout(
+  () => {
+    console.log('__callbacks as argument__');
+    const oCb = observerCallbacks;
+    const sub = observable.subscribe(oCb.next, oCb.error, oCb.complete);
+    observableComplete.subscribe(oCb.next, oCb.error, oCb.complete);
+    observableError.subscribe(oCb.next, oCb.error, oCb.complete);
+    // unsub
+    setTimeout(() => sub.unsubscribe(), 1100);
+  },
+  4600,
+);
