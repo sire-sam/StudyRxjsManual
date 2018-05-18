214c6189a354c4602ee20c06be9674fa1b973f1e
sire-sam <sire-sam@users.noreply.github.com>
2018-05-18 21:42:29 +0200

    End Observable - Anatomy of an Observable
    From RxJs manual
    http://reactivex.io/rxjs/manual/overview.html#anatomy-of-an-observable

--- app/app.js
+++ app/app.js
@@ -1,80 +1,87 @@
 import { Observable } from 'rxjs';
 
-// Observable
-// http://reactivex.io/rxjs/manual/overview.html#observable
+// Anatomy of an Observable
+// http://reactivex.io/rxjs/manual/overview.html#anatomy-of-an-observable
 
-// Intro Example
+// Subscribing to Observables
 //
-const observable = Observable.create((observer) => {
-  observer.next(1);
-  observer.next(2);
-  observer.next(3);
-  setTimeout(() => {
-    observer.next(4);
+// create:argument is a "subscribe" function aka:
+// Calling observable.subscribe with an Observer,
+// the function "subscribe" in Observable.create() is run for that given Observe
+// eslint-disable-next-line
+const obs = Observable.create(function subscribe(observer) {
+  setInterval(
+    () => {
+      console.log('interval execution');
+      observer.next('hi');
+    },
+    1000,
+  );
+});
+const intervalSubs = obs.subscribe(x => console.log(x));
+
+// Executing Observables
+//
+// next*(error|complete)?
+// zero to infinite Next notifications may be delivered.
+// If either Error or Complete is delivered, then nothing else can be delivered afterwards.
+const obs2 = Observable.create((observer) => {
+  try {
+    observer.next(1);
+    observer.next(2);
+    observer.next(3);
     observer.complete();
+    observer.next(4); // not delivered
+  } catch (e) {
+    observer.error(e);
+  }
+});
+obs2.subscribe(x => console.log(x));
+
+// Disposing Observable Executions
+//
+// subscribe() return Subscription (the ongoing execution), call unsubscribe() cancel the execution
+
+// __Important:__
+// --------------
+// Observable must define how to dispose resources of that execution
+// exemple: even if we did unsubscribe for previously defined intervalSubs,
+// interval still exists, see loging of "interval execution" in consol
+setTimeout(() => intervalSubs.unsubscribe(), 4000);
+
+//  return a custom unsubscribe function from within function subscribe() to cleanup.
+const obs3 = Observable.create((observer) => {
+  const id = setInterval(
+    () => {
+      console.log('interval too execution');
+      observer.next('hi too');
+    },
+    1000,
+  );
+
+  return function unsubscribe() {
+    clearInterval(id);
+  };
+});
+const intervalTooSub = obs3.subscribe(x => console.log(x));
+setTimeout(() => {
+  intervalTooSub.unsubscribe();
+  console.log('no more "interval too" logging');
+}, 4000);
+
+// __Note__
+// This is pretty simple pattern
+const straightSubscribe = function subscribe(observer) {
+  const id = setInterval(() => {
+    observer.next('hi straight');
   }, 1000);
-});
 
+  return function unsubscribe() {
+    clearInterval(id);
+  };
+};
+const straightUnsubscribe = straightSubscribe({ next: x => console.log(x) });
+setTimeout(() => straightUnsubscribe(), 4000);
 
-console.log('just before subscribe');
-observable.subscribe({
-  next: x => console.log('next value', x),
-  error: err => console.log('error', err),
-  complete: () => console.log('done'),
-});
-
-// Observables as generalizations of functionsObservables as generalizations of functions
-// http://reactivex.io/rxjs/manual/overview.html#observables-as-generalizations-of-functions
-
-// Observable subscription are like function call
-// they are not like events, one subscrive trigger a "call"
-const observable2 = Observable.create((observer) => {
-  console.log('hello');
-  observer.next(42);
-});
-
-
-observable2.subscribe(x => console.log(x));
-observable2.subscribe(y => console.log(y));
-
-// -> Observable are not asynchronous by default,
-// they are synchronous just like function
-// proved by following whre subscribe 'call' is logged between "before" and "after" log
-// if it was asynchrnous logs from observer and subscriber would be displayed after "after" log
-console.log('Before');
-observable2.subscribe(x => console.log(x));
-console.log('After');
-
-// Observable can return multiple value something a function can't do (only one return statement)
-// but still "synchronously"
-const multipleOutput = Observable.create((observer) => {
-  console.log('Hello');
-  observer.next(42);
-  observer.next(100);
-  observer.next(200);
-  observer.complete();
-});
-console.log('before multipleOutput');
-multipleOutput.subscribe(output => console.log('multiple output', output));
-console.log('after multipleOutput');
-
-// -> but they can "push" values asynchronously
-const multiAsync = Observable.create((observer) => {
-  console.log('Hello, one will be async');
-  observer.next(42);
-  observer.next(100);
-  observer.next(200);
-  setTimeout(()=> observer.next(300),1000)
-  observer.next(400);
-})
-console.log('before multiAsync');
-multiAsync.subscribe(output => console.log('multiAsync output', output));
-console.log('after multiAsync');
-
-/*
-Conclusion:
-
-func.call() means "give me one value synchronously"
-observable.subscribe() means "give me any amount of values, either synchronously or asynchronously"
-
- */
+// The reason we use Rx types like Observable, Observer, and Subscription
+// is to get safety (such as the Observable Contract) and composability with Operators.
