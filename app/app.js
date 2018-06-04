a5870d0ee6de9d42d436427fe351fc8b01296caf
sire-sam <sire-sam@users.noreply.github.com>
2018-06-04 22:05:58 +0200

    End Subject - Multicasted Observables Connect

--- app/app.js
+++ app/app.js
@@ -1,39 +1,21 @@
-import { Observable, Subject, from } from 'rxjs';
+import { Subject, interval } from 'rxjs';
 import { multicast } from 'rxjs/operators';
 
-// Subject
-// http://reactivex.io/rxjs/manual/overview.html#subject
+// Subject - Multicasted Observables
+const src = interval(500);
 const subj = new Subject();
+const multicasted = src.pipe(multicast(subj));
+let sub2;
 
-// Every Subject is an Observable
-//
-subj.subscribe(x => console.log('a', x));
-subj.subscribe({ next: x => console.log('b', x) });
+const sub1 = multicasted.subscribe(next => console.log(`sub1: next: ${next}`));
+const subConnect = multicasted.connect();
 
-// Every Subject is an Observer
-//
-subj.next(1);
-subj.next(2);
+setTimeout(() => {
+  sub2 = multicasted.subscribe(next => console.log(`sub2: next: ${next}`));
+}, 600);
 
-// Subject is an Observer so you may provide a Subject as observer of another Observable
-const obs = from([3, 4, 5]);
-
-obs.subscribe(subj);
-
-// Note:
-// Subjects are the only way of making any Observable execution be shared to multiple Observers
-
-// Multicasted Observables intro
-//
-// multicasted Observable uses a Subject to make Observers see the same Observable execution
-// under the hood
-const src = from([1, 2, 3]);
-const subjMulticast = new Subject();
-const multicasted = src.pipe(multicast(subjMulticast));
-
-multicasted.subscribe(x => console.log('multiCasted A', x));
-multicasted.subscribe(y => console.log('multiCasted B', y));
-
-// same result as `obs.subscribe(subj)` in previous example
-multicasted.connect();
-
+setTimeout(() => sub1.unsubscribe(), 1200);
+setTimeout(() => {
+  sub2.unsubscribe();
+  subConnect.unsubscribe();
+}, 2000);
